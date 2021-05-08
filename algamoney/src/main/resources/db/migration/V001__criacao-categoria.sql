5cef17559e7020ae29381f67265463ca7c01d318
Reysson <reysson.joseph29@gmail.com>
2021-05-08 14:40:42 -0400

    Conectando com o banco postgresql

---
+++ algamoney/src/main/resources/db/migration/V001__criacao-categoria.sql
@@ -0,0 +1,5 @@
+CREATE TABLE categoria (
+    codigo SERIAL,
+    nome VARCHAR(50) NOT NULL,
+    PRIMARY KEY(codigo)
+);
---
+++ algamoney/src/main/resources/db/testedata/afterMigrate.sql
@@ -0,0 +1,10 @@
+TRUNCATE categoria CASCADE;
+
+ALTER SEQUENCE categoria_codigo_seq RESTART WITH 1;
+
+INSERT INTO categoria (nome) values ('Lazer');
+INSERT INTO categoria (nome) values ('Alimentação');
+INSERT INTO categoria (nome) values ('Supermercado');
+INSERT INTO categoria (nome) values ('Farmácia');
+INSERT INTO categoria (nome) values ('Outros');
+
