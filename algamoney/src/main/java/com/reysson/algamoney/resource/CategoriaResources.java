e5d274487c11406f73bd47268a499d8c8db50e3f
Reysson <reysson.joseph29@gmail.com>
2021-05-09 18:32:18 -0400

    Salvando categorias

--- algamoney/src/main/java/com/reysson/algamoney/resource/CategoriaResources.java
+++ algamoney/src/main/java/com/reysson/algamoney/resource/CategoriaResources.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,24 +18,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResources {
-    
+
     @Autowired
     private CategoriaRepository categoriaRepository;
-    
-    
-    
+
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<Categoria> listarTodas(){
+    public List<Categoria> listarTodas() {
         return categoriaRepository.findAll();
     }
-    
+
     @GetMapping("/{id}")
-    public ResponseEntity<?> buscarPorId(@PathVariable Integer id){
+    public ResponseEntity<?> buscarPorId(@PathVariable Integer id) {
         Optional<Categoria> categoria = categoriaRepository.findById(id);
-        if(categoria.isEmpty()){
+        if (categoria.isEmpty()) {
             return ResponseEntity.notFound().build();
-        } 
+        }
         return ResponseEntity.ok(categoria.get());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Categoria salvar(@RequestBody Categoria categoria) {
+        return categoriaRepository.save(categoria);
+
     }
 }
